fba29078bfe6c10819a22d4f0ff7d2dc5c753e23
Quinn Cheong <quinncheong@hotmail.com>
2021-08-20 14:21:50 +0800

    Add in eslint scripts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/tailwind.css';
 
-var x = 'op'
+const x = 'op';
+console.log('whoper');
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app');
