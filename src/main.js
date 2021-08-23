7a714a1c90ca55bf16cccc8ecbd0db43b2a01437
Quinn Cheong <quinncheong@hotmail.com>
2021-08-23 12:55:54 +0800

    Changed eslintrc env and root

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
 
-const x = 'op';
+// const x = 'op';
 console.log('whoper');
+console.log('op');
 
 createApp(App)
 	.use(router)
